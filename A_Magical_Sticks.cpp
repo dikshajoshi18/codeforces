42140dc91c45a4217756f03883ecf081cccd72f9
dikshajoshi18 <dikshajoshi1800@gmail.com>
2020-10-12 23:32:49 +0530

    new

---
+++ A_Magical_Sticks.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+        cout<<(n+1)/2<<endl;
+    }
+}
